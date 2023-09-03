9bc663b1f028e1c401092d54cf1fb3ed802a4b5b
Daniil <70702619+danial29rus@users.noreply.github.com>
2023-09-03 18:21:09 +0300

    test_amvera

--- server/app/main.py
+++ server/app/main.py
@@ -2,6 +2,7 @@ import ipaddress
 
 from fastapi import FastAPI, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
+from starlette.middleware.base import BaseHTTPMiddleware
 
 from app.database import engine
 from app.endpoints.cdek_calc.rout import calculator_cdek
@@ -15,7 +16,7 @@ from fastapi.templating import Jinja2Templates
 
 
 async def not_found_error(request: Request, exc: HTTPException):
-    return templates.TemplateResponse("404.html", {"request": request})
+    return templates.TemplateResponse("templates/404.html", {"request": request})
 
 
 exception_handlers = {404: not_found_error}
@@ -24,16 +25,21 @@ app = FastAPI(
     title="Cdek 21",
     exception_handlers=exception_handlers,
 )
+
+
 @app.exception_handler(HTTPException)
 async def not_found_error(request: Request, exc: HTTPException):
     if exc.status_code == 404 or request.url.path == "/admin/":
-        return templates.TemplateResponse("404.html", {"request": request})
+        return templates.TemplateResponse("templates/404.html", {"request": request})
     raise exc
+
+
 templates = Jinja2Templates(directory="app/public/")
 app.mount("/", StaticFiles(directory="app/public/", html=True), name="static")
 admin = Admin(app, engine)
 app.include_router(router)
 app.include_router(calculator_cdek)
+
 
 
 @app.middleware("http")
@@ -42,8 +48,6 @@ async def check_admin_access(request: Request, call_next):
     password = request.query_params.get("password")
     if user == "cdek21" and password == "cdek21password":
         return RedirectResponse(url="/admin/")
-
-
 
     response = await call_next(request)
     return response
@@ -54,8 +58,6 @@ class Email_formAdmin(ModelView, model=Email_form):
 
 
 admin.add_view(Email_formAdmin)
-
-
 
 
 @app.get("/")
